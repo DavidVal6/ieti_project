d45bcb45ec457ad5af31a09a05f88326c3bca551
SanRocks1220 <xxsantirocha12xx@gmail.com>
2024-02-25 21:00:31 -0500

    MongoDB connection stablished

--- src/main/java/edu/eci/ieti/proyecto/repositories/PlantRepository.java
+++ src/main/java/edu/eci/ieti/proyecto/repositories/PlantRepository.java
@@ -1,11 +1,11 @@
 package edu.eci.ieti.proyecto.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import edu.eci.ieti.proyecto.data.Plant;
 
 @Repository
-public interface PlantRepository extends JpaRepository<Plant, Long>{
+public interface PlantRepository extends MongoRepository<Plant, Long>{
     
 }
